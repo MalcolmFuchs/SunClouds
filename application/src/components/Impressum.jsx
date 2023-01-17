96126dbce2a341f9b370d8816e181e7b88a99fee
Aleks-ic <82808424+Aleks-ic@users.noreply.github.com>
2023-01-17 16:22:20 +0100

    Hochschuladresse, Mail und Verantwortliche hinzugefügt

--- application/src/components/Impressum.jsx
+++ application/src/components/Impressum.jsx
@@ -8,6 +8,14 @@ export default function Impressum() {
       <h1>Impressum</h1>
       <p>Malcolm Fuchs & Aleksandar Ilic</p>
       <p>Hochschule RuhrWest</p>
+      <p>Campus Mülheim</p>
+      <p>Duisburger Str. 100</p>
+      <p>45479 Mülheim an der Ruhr</p>
+    
+      <p>Mail: malcolm.fuchs@stud.hs-ruhrwest.de</p>
+      <p>Mail: aleksandar.ilic@stud.hs-ruhrwest.de</p>
+    
+      <p>Verantwortliche: Malcolm Fuchs & Aleksandar Ilic</p>
     </div>
   );
 
