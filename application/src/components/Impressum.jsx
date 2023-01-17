1c05dc38e1d981adc9bd02dbd442cab3d3144445
Aleks-ic <82808424+Aleks-ic@users.noreply.github.com>
2023-01-17 16:27:06 +0100

    Umbrüche hinzugefügt und bearbeitet

--- application/src/components/Impressum.jsx
+++ application/src/components/Impressum.jsx
@@ -6,15 +6,17 @@ export default function Impressum() {
   return (
     <div className="Impressum">
       <h1>Impressum</h1>
-      <p>Malcolm Fuchs & Aleksandar Ilic</p>
+      <br />
+      <h1>SunClouds</h1>
+      <br />
       <p>Hochschule RuhrWest</p>
       <p>Campus Mülheim</p>
       <p>Duisburger Str. 100</p>
       <p>45479 Mülheim an der Ruhr</p>
-    
+      <br />
       <p>Mail: malcolm.fuchs@stud.hs-ruhrwest.de</p>
       <p>Mail: aleksandar.ilic@stud.hs-ruhrwest.de</p>
-    
+      <br />
       <p>Verantwortliche: Malcolm Fuchs & Aleksandar Ilic</p>
     </div>
   );
